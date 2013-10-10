077ea034041d1ec9b675d1bccfa7a3c37142142b
daerty0153 <kartik.godawat@gmail.com>
2013-10-11 00:38:49 +0530

    Added django

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='HN',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django>=1.3'],
+      description='HN App',
+      author='daerty0153',
+      author_email='kartik.godawat@gmail.com',
+      url='http://github.com/daerty0153',
+      install_requires=['Django=1.5.4'],
      )
